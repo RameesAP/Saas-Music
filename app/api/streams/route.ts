c5ccccce7f8f93382d70132d89238073b33766e6
Ramees <rameeswork@gmail.com>
2024-10-08 23:52:17 +0530

    1:24 update

---
+++ app/api/streams/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import { prismaClient } from "@/app/lib/db";
+
+const YT_REGEX = new RegExp("^https://www.youtube.com/watch?v=[w-]{11}$");
+
+const CreateStreamSchema = z.object({
+  createrId: z.string(),
+  url: z.string(),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const data = CreateStreamSchema.parse(await req.json());
+    const isYt = YT_REGEX.test(data.url);
+    if (!isYt) {
+      return NextResponse.json(
+        {
+          message: "Wrong url format",
+        },
+        {
+          status: 411,
+        }
+      );
+    }
+
+    const extractedId = data.url.split("?v=")[1];
+
+    prismaClient.stream.create({
+      data: {
+        userId: data.createrId,
+        url: data.url,
+        extractedId,
+        type:"Youtube"
+      },
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error while adding a stream",
+      },
+      {
+        status: 411,
+      }
+    );
+  }
+}
